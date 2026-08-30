b0c50324a8b8d98fcdb895f6499a403b971c68b1
aleksandr.kursin <beardkid7@gmail.com>
2026-08-30 14:45:51 +0300

    stats - Правки после api git tests

--- stats-service/stats-server/src/main/java/ru/yandex/practicum/ewm/controller/ErrorHandler.java
+++ stats-service/stats-server/src/main/java/ru/yandex/practicum/ewm/controller/ErrorHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.ewm.exception.ConflictedDataException;
+import ru.yandex.practicum.ewm.exception.ValidateDataException;
 import ru.yandex.practicum.ewm.model.ErrorResponse;
 
 
@@ -12,8 +12,8 @@ import ru.yandex.practicum.ewm.model.ErrorResponse;
 public class ErrorHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleConflictData(final ConflictedDataException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConflictData(final ValidateDataException e) {
         return new ErrorResponse(e.getMessage());
     }
 }
--- stats-service/stats-server/src/main/java/ru/yandex/practicum/ewm/exception/ConflictedDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.ewm.exception;
-
-public class ConflictedDataException extends RuntimeException {
-    public ConflictedDataException(String message) {
-        super(message);
-    }
-}
